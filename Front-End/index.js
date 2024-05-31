f16afda154a850f11ab81e0f4b7ddcab13a16602
caio fortes <caiofortesprofissional@gmail.com>
2024-05-31 12:57:14 -0300

    excluindo pastas

--- Front-End/index.js
+++
@@ -1,8 +0,0 @@
-import "@fontsource/m-plus-rounded-1c"; 
-import "@fontsource/m-plus-rounded-1c/400.css";
-import "@fontsource/m-plus-rounded-1c/400-italic.css"; 
-
-import "@fontsource/open-sans"; 
-import "@fontsource/open-sans/500.css"; // Weight 500.
-import "@fontsource/open-sans/900-italic.css"; // Italic variant.
-
